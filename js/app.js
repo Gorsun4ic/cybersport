f187fe464c75cb5648849813907fb65f8b8414ad
Gorsun4ic <103429653+Gorsun4ic@users.noreply.github.com>
2022-08-09 01:31:28 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -196,7 +196,7 @@
                 const animStart = 4;
                 let animItemPoint = window.innerHeight - animItemHeight / animStart;
                 if (animItemHeight > window.innerHeight) animItemPoint = window.innerHeight - window.innerHeight / animStart;
-                if (pageYOffset > animItemOffset - animItemPoint && pageYOffset < animItemOffset + animItemHeight) animItem.classList.add("_active"); else if ();
+                if (pageYOffset > animItemOffset - animItemPoint && pageYOffset < animItemOffset + animItemHeight) animItem.classList.add("_active"); else if (!animItem.classList.contains("_anim-no-hide")) ;
             }
         }
         function offset(el) {
